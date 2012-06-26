a784a0494b8d43240850bae26356f474cd0389e3
John K. Parejko <John.Parejko@alumni.carleton.edu>
2012-06-26 13:48:52 -0400

    Added ds9 region file output to do_sex.py
    Added parameters to sextractor parameter file.

--- python/do_sex.py
+++ python/do_sex.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 """
 Run Sextractor on simulated files and compare with known positions.
 """
@@ -20,12 +20,60 @@ def run_sex(filename):
     Note that sextractor sees the points in pixel xy coordinates,
     and the axes are flipped.
     """
-    outfile = os.path.splitext(filename)[0]+'_sex.fits'
+    outfile = '../data/sex/'+os.path.splitext(os.path.basename(filename))[0]+'_sex.fits'
+    logfile = '../temp/'+os.path.basename(outfile)
     cmd = ' '.join(('sex',filename,'-CATALOG_NAME',outfile))
-    stdout=open('../temp/'+outfile+'.log','w')
-    stderr=open('../temp/'+outfile+'.err','w')
+    stdout=open(logfile+'.log','w')
+    stderr=open(logfile+'.err','w')
     subprocess.call(cmd,shell=True,stdout=stdout,stderr=stderr)
     return np.array(pyfits.open(outfile)[1].data),outfile
+#...
+
+def make_regions(measured,sexfilename):
+    """Make a ds9 region file from the sextractor output."""
+    try:
+        x = measured['X_IMAGE_DBL']
+        y = measured['Y_IMAGE_DBL']
+    except KeyError:
+        x = None
+        y = None
+    try:
+        a = measured['A_IMAGE']
+        b = measured['B_IMAGE']
+        t = measured['THETA_IMAGE']
+    except KeyError:
+        a = None
+        b = None
+        t = None
+    try:
+        xpeak = measured['XPEAK_IMAGE']
+        ypeak = measured['YPEAK_IMAGE']
+    except KeyError:
+        xpeak = None
+        ypeak = None
+    try:
+        flux = measured['FLUX_ISO']
+    except KeyError:
+        flux = None
+
+    if a is not None:
+        outfile = open(sexfilename.replace('.fits','-ellipses.reg'),'w')
+        # write some default parameters
+        outfile.write('global color=cyan font="helvetica 10 normal" edit=1 move=0 delete=1 include=1\n')
+        formatter = lambda x,y,a,b,t,flux: 'ellipse('+','.join((str(x),str(y),str(a),str(b),str(t)))+') #text="%.2e"'%flux
+        outfile.write('\n'.join(map(formatter,x,y,a,b,t,flux)))
+        outfile.write('\n')
+        formatter = lambda x,y: 'point('+','.join((str(x),str(y)))+') #point=cross width=2'
+        outfile.write('\n'.join(map(formatter,x,y)))
+        outfile.close()
+    if xpeak is not None:
+        outfile = open(sexfilename.replace('.fits','-peaks.reg'),'w')
+        # write some default parameters
+        outfile.write('global color=red font="helvetica 12 normal" edit=1 move=0 delete=1 include=1 width=2\n')
+        formatter = lambda xpeak,ypeak: 'point('+','.join((str(xpeak),str(ypeak)))+') #point=x'
+        outfile.write('\n'.join(map(formatter,xpeak,ypeak)))
+        outfile.write('\n')
+        outfile.close()
 #...
 
 def read_positions():
@@ -37,6 +85,10 @@ def read_positions():
     return points
 #...
 
+# !!!!!!!!!!!!!!!!!!!!
+# TBD: this isn't the right way to do it!
+# Add support for fvmapping.Transform
+# !!!!!!!!!!!!!!!!!!!!
 def match_positions(data,points):
     """
     Match to the positions in Baltay's files.
@@ -94,7 +146,7 @@ def plot_hist(dist,flux):
     pylab.ylim((0,40))
     pylab.xlabel('separation (pixels)')
     pylab.ylabel('Number')
-    pylab.savefig('testimage_matches.png')
+    #pylab.savefig('testimage_matches.png')
 #...
 
 def main(argv=None):
@@ -104,12 +156,9 @@ def main(argv=None):
     usage = '%prog [OPTIONS] FITSFILES'
     usage += '\nRun Sextractor on files and compare with known positions.'
     parser = OptionParser(usage)
-    parser.add_option('-s','--save',dest='save',action='store_true',default=False,
-                      help='Save the sextractor output .fits files (%default).')
 
     (opts,args) = parser.parse_args(argv)
 
-    print args
     if len(args) == 0:
         parser.error('Need to pass some fits image files!')
 
@@ -121,13 +170,12 @@ def main(argv=None):
         name = os.path.splitext(f)[0].split('testimage_')[-1]
         print 'Processing:',name
         measured,filename = run_sex(f)
+        make_regions(measured,filename)
         dist[name] = match_positions(measured,points)
         flux[name] = np.mean(measured['FLUX_ISO'])        
         print 'Matches found:',len(dist[name])
         print 'Flux and sigma(flux):',flux[name],np.std(measured['FLUX_ISO'])
         print
-        if not opts.save:
-            os.remove(filename)
 
     plot_hist(dist,flux)
 #...
